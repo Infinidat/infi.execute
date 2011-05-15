4dc54830de9d9d83e5814f87a1ec9a7a51f0b0b7
Rotem Yaari <vmalloc@gmail.com>
2011-05-15 21:12:33 +0300

    Fix packaging

--- setup.py
+++ setup.py
@@ -1,24 +1,9 @@
 
 import os
 import itertools
-from setuptools import setup
+from setuptools import setup, find_packages
 
 from infi.execute import __version__ as VERSION
-
-def _find_all_subpackages(*packages):
-    for root_package in packages:
-        root_dir = os.path.dirname(__file__)
-        package_root = os.path.abspath(os.path.join(root_dir, root_package))
-        yield root_package
-        for directory, dirnames, filenames in os.walk(package_root):
-            if "__init__.py" not in filenames:
-                del dirnames[:]
-                continue
-            relative_package = os.path.relpath(directory, package_root).replace(os.path.sep, ".")
-            if relative_package == '.':
-                continue
-            yield root_package + "." + relative_package
-
 
 setup(name="infi.execute",
       classifiers = [
@@ -33,7 +18,8 @@ setup(name="infi.execute",
       author_email="",
       #url="your.url.here",
       version=VERSION,
-      packages=list(_find_all_subpackages("infi")),
+      packages=find_packages(exclude=["tests"]),
+      namespace_packages=["infi"],
       install_requires=["pyforge"],
       scripts=[],
       )
