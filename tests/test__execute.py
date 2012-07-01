efdd979bac20192188b30363ac44b1753768ab4c
Guy Rozendorn <guy@rzn.co.il>
2012-07-01 09:33:07 +0300

    TRIVIAL increasing delta to make tests pass
    When the VMs are loaded, the current delta is too short

--- tests/test__execute.py
+++ tests/test__execute.py
@@ -19,7 +19,7 @@ class TestCase(test_utils.TestCase):
     def assertImmediate(self):
         return self.assertTakesAlmost(0)
     @contextmanager
-    def assertTakesAlmost(self, secs, delta=0.3):
+    def assertTakesAlmost(self, secs, delta=0.5):
         start_time = time()
         yield None
         self.assertLess(abs(time()-(start_time + secs)), delta)
